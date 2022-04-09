dc589e60ca8f31210fd5543470b7159f2fb03007
Bbold72 <reinbold.brian.j@gmail.com>
2022-04-09 17:35:17 -0500

    add CauseSpecific netowork to models

--- baselines/models.py
+++ baselines/models.py
@@ -1,4 +1,5 @@
 import torch 
+import torchtuples as tt # Some useful functions
 
 
 def simple_dln(config):
@@ -19,3 +20,36 @@ def simple_dln(config):
         torch.nn.Linear(hidden_size, config.out_feature)
     )
     return net
+
+class CauseSpecificNet(torch.nn.Module):
+    """Network structure similar to the DeepHit paper, but without the residual
+    connections (for simplicity).
+    """
+    def __init__(self, config, batch_norm=True):
+        dropout = config.dropout
+        num_nodes_shared = config.hidden_size_shared
+
+        super().__init__()
+        self.shared_net = tt.practical.MLPVanilla(
+            config.num_feature, 
+            num_nodes_shared, 
+            num_nodes_shared,
+            batch_norm, 
+            dropout,
+        )
+        self.risk_nets = torch.nn.ModuleList()
+        for _ in range(config.num_event):
+            net = tt.practical.MLPVanilla(
+                num_nodes_shared, 
+                config.hidden_size_indiv, 
+                config.out_feature,
+                batch_norm, 
+                dropout,
+            )
+            self.risk_nets.append(net)
+
+    def forward(self, input):
+        out = self.shared_net(input)
+        out = [net(out) for net in self.risk_nets]
+        out = torch.stack(out, dim=1)
+        return out
