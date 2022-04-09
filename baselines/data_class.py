3e0c5a26462654198ac22693098553211c9d8721
Bbold72 <reinbold.brian.j@gmail.com>
2022-04-09 17:34:43 -0500

    add competing events to data_class

--- baselines/data_class.py
+++ baselines/data_class.py
@@ -9,7 +9,7 @@ class Data:
         self.df, self.df_train, self.df_y_train, self.df_test, self.df_y_test, self.df_val, self.df_y_val = load_data(config)
 
         # censor event
-        if config.data == 'seer':
+        if config.data == 'seer' and config.model == 'PCHazard':
 
             censor_event = lambda df: df.drop(config.event_to_censor, axis=1).rename(columns={event_to_keep: 'event'})
 
@@ -20,6 +20,25 @@ class Data:
             self.df_y_train = censor_event(self.df_y_train)
             self.df_y_val = censor_event(self.df_y_val)
             self.df_y_test = censor_event(self.df_y_test)
+
+        # competing events
+        if config.model == 'DeepHitCompeting':
+            # make event variable for pycox model
+            # 0: right censored
+            # 1: Heart Disease (event_0)
+            # 2: Breast Cancer (event_1)
+            def make_event_col(df):
+                df['event'] = np.where(df['event_0'] == 1,
+                                        1,
+                                        np.where(df['event_1'] == 1,
+                                                2,
+                                                0
+                                                )
+                                        )
+                return df
+
+            self.df, self.df_y_train, self.df_y_val, self.df_y_test = map(make_event_col, [self.df, self.df_y_train, self.df_y_val, self.df_y_test])
+    
 
         # convet data to format necessary for pycox
         self.x_train = np.array(self.df_train, dtype='float32')
