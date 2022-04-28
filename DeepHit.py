ab54097d3ede6437afe5c0d79351b25c550a1120
Bbold72 <reinbold.brian.j@gmail.com>
2022-04-27 21:43:46 -0500

    refactor DeepHit models

--- DeepHit.py
+++ DeepHit.py
@@ -1,16 +1,11 @@
-from easydict import EasyDict
-import numpy as np
 import time
 
-import torchtuples as tt # Some useful functions
-
-from pycox.models import DeepHitSingle, DeepHit
 
 from baselines.data_class import Data
-from baselines.models import simple_dln, CauseSpecificNet
 from baselines.evaluator import EvaluatorSingle, EvaluatorCompeting
 from baselines.utils import export_results, update_run
 from baselines import configurations
+from baselines.models import DeepHitSingleEvent, DeepHitCompeting
 
 
 num_runs = 10
@@ -32,44 +27,22 @@ for dataset_name in datasets:
         # load data
         data = Data(config)
 
-        # define neural network
+        # initialize model
         if config.data == 'seer':
-            net = CauseSpecificNet(config)
-            optimizer = tt.optim.AdamWR(lr=0.01, decoupled_weight_decay=0.01,
-                                cycle_eta_multiplier=0.8)
-            # initialize model
-            model = DeepHit(net, optimizer, 
-                            alpha=0.2, 
-                            sigma=0.1,
-                            duration_index=config.duration_index
-                            )
-            
+            model = DeepHitCompeting(config)
             Evaluator = EvaluatorCompeting
         else:
-            net = simple_dln(config)
-
-            # initialize model
-            model = DeepHitSingle(net, tt.optim.Adam, 
-                                    alpha=0.2, 
-                                    sigma=0.1, 
-                                    duration_index=np.array(config['duration_index'],
-                                    dtype='float32')
-                                    )
-            model.optimizer.set_lr(config.learning_rate)
-
+            model = DeepHitSingleEvent(config)
             Evaluator = EvaluatorSingle
-
-        # add early stopping
-        callbacks = [tt.callbacks.EarlyStopping(patience=20)]
 
         # train model
         train_time_start = time.time()
-        log = model.fit(data.x_train, data.y_train, config.batch_size, config.epochs, callbacks, val_data=data.val_data)
+        log = model.train(data)
         train_time_finish = time.time()
 
 
         # calcuate metrics
-        evaluator = Evaluator(data, model, config, offset=0)
+        evaluator = Evaluator(data, model.model, config, offset=0)
         run = evaluator.eval()
         run = update_run(run, train_time_start, train_time_finish, log.epoch)
 
--- baselines/models.py
+++ baselines/models.py
@@ -2,10 +2,11 @@ import numpy as np
 import torchtuples as tt # Some useful functions
 
 from sksurv.linear_model import CoxPHSurvivalAnalysis
+from pycox.models import DeepHitSingle, DeepHit
 from pycox.models import PCHazard as PCH
 from sksurv.ensemble import RandomSurvivalForest
 
-from baselines.dlns import simple_dln
+from baselines.dlns import simple_dln, CauseSpecificNet
 
 
 
@@ -18,6 +19,53 @@ class CPH:
     def train(self, data):
         self.model.fit(data.x_train, data.y_et_train)
   
+
+class DeepHitCompeting:
+
+    def __init__(self, config):
+
+        self.batch_size = config.batch_size
+        self.epochs = config.epochs
+        net = CauseSpecificNet(config)
+        optimizer = tt.optim.AdamWR(lr=0.01, 
+                                        decoupled_weight_decay=0.01,
+                                        cycle_eta_multiplier=0.8
+                                        )
+        self.callbacks = [tt.callbacks.EarlyStopping(patience=20)]
+
+        # initialize model
+        self.model = DeepHit(net, optimizer, 
+                        alpha=0.2, 
+                        sigma=0.1,
+                        duration_index=config.duration_index
+                        )
+        
+    def train(self, data):
+        log = self.model.fit(data.x_train, data.y_train, self.batch_size, self.epochs, self.callbacks, val_data=data.val_data)
+        return log
+            
+    
+class DeepHitSingleEvent:
+
+    def __init__(self, config):
+
+        self.batch_size = config.batch_size
+        self.epochs = config.epochs
+        net = simple_dln(config)
+        self.callbacks = [tt.callbacks.EarlyStopping(patience=20)]
+
+        # initialize model
+        self.model = DeepHitSingle(net, tt.optim.Adam, 
+                                alpha=0.2, 
+                                sigma=0.1, 
+                                duration_index=np.array(config['duration_index'],
+                                dtype='float32')
+                                )
+        self.model.optimizer.set_lr(config.learning_rate)
+        
+    def train(self, data):
+        log = self.model.fit(data.x_train, data.y_train, self.batch_size, self.epochs, self.callbacks, val_data=data.val_data)
+        return log
 
 
 class PCHazard:
@@ -44,6 +92,7 @@ class PCHazard:
                             val_data=data.val_data
                             )
         return log
+
 
 class RSF:
 
